669cac43f284dbcd4cb502fc0597b616d05cac21
Russell Davidson <rbdavid@illinois.edu>
2025-03-11 15:57:42 -0500

    remove calls for sleep

--- batch_scripts/dask_batch.sh
+++ batch_scripts/dask_batch.sh
@@ -43,18 +43,11 @@ echo "Spinning up the Scheduler"
 dask scheduler --no-dashboard --no-jupyter --no-show --scheduler-file ${SCHEDULER_FILE} --interface 'eth0' > dask_scheduler.out 2>&1 &
 dask_pids="$dask_pids $!"
 
-# needed to ensure the scheduler has actually spun up before workers begin to be spun up. 
-# like if writing the SCHEDULER_FILE to its path is slow, then the `dask worker` call may not start up the communications between workers and the scheduler
-sleep 20
-
 # spin up nWorkers workers, each with 1 core
 echo "Spinning up the Workers"
 srun -n 62 \
 	dask worker --no-dashboard --no-nanny --reconnect --nthreads 1 --nworkers 1 --interface 'eth0' --scheduler-file ${SCHEDULER_FILE} > dask_worker.out 2>&1 &
 dask_pids="$dask_pids $!"
-
-# do it again to make sure workers are communicating with the scheduler and vice versa
-sleep 20
 
 # run the workflow on all ENA files in $ENA_PATH
 echo "Starting Client Script"
