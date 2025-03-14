2330a49c68197f668b92b5df3aabaacfe470491a
Russell Davidson <rbdavid@illinois.edu>
2025-03-14 10:10:36 -0500

    more clean up of batch script

--- batch_scripts/dask_batch.sh
+++ batch_scripts/dask_batch.sh
@@ -18,19 +18,25 @@ conda activate ena_db_build
 dask_pids=""
 
 echo "Spinning up the Scheduler"
-SCHEDULER_FILE=$WORKING_DIR/scheduler_file.json
-dask scheduler --no-dashboard --no-jupyter --no-show --scheduler-file ${SCHEDULER_FILE} --interface 'eth0' > dask_scheduler.out 2>&1 &
+dask scheduler $dask_scheduler_arguments > dask_scheduler.out 2>&1 &
 dask_pids="$dask_pids $!"
 
 # spin up N_WORKERS workers, each with 1 core
 echo "Spinning up the Workers"
 srun -n $N_WORKERS \
-	dask worker --no-dashboard --no-nanny --reconnect --nthreads 1 --nworkers 1 --interface 'eth0' --scheduler-file ${SCHEDULER_FILE} > dask_worker.out 2>&1 &
+	dask worker $dask_worker_arguments > dask_worker.out 2>&1 &
 dask_pids="$dask_pids $!"
 
 # run the workflow on all ENA files in $EBROOTENA
 echo "Starting Client Script"
-python3 ena_dask_tskmgr --db-config $DB_CONFIG --db-name $DB_NAME --ena-paths $EBROOTENA/sequence $EBROOTENA/wgs/public  $EBROOTENA/wgs/suppressed  --output-dir $OUTPUT_DIR --local-scratch $SCRATCH_DIR --scheduler-file ${SCHEDULER_FILE} --n-workers $N_WORKERS --tskmgr-log-file $WORKING_DIR/tskmgr.log
+python3 ena_dask_tskmgr --db-config $DB_CONFIG \
+			--db-name $DB_NAME \
+			--ena-paths $EBROOTENA/sequence $EBROOTENA/wgs/public  $EBROOTENA/wgs/suppressed \
+			--output-dir $OUTPUT_DIR \
+			--local-scratch $SCRATCH_DIR \
+			--scheduler-file ${SCHEDULER_FILE} \
+			--n-workers $N_WORKERS \
+			--tskmgr-log-file $WORKING_DIR/tskmgr.log
 
 for pid in $dask_pids
 do
--- batch_scripts/env_setup.sh
+++ batch_scripts/env_setup.sh
@@ -27,3 +27,12 @@ DB_CONFIG=/path/to/mysql/webserver/config/file
 DB_NAME=efi_202412	# or the equivalent version string for the relevant MySQL EFI Database to be queried
 N_WORKERS=$((SLURMNTASKS - 2)) # scheduler and client script both need at least one cpu each.
 
+# dask architecture settings
+# see https://docs.dask.org/en/latest/deploying-cli.html
+scheduler_file=$WORKING_DIR/scheduler_file.json
+dask_interface="eth0"
+nThreads_per_worker=1
+dask_scheduler_arguments="--no-dashboard --no-jupyter --no-show --scheduler-file ${SCHEDULER_FILE} --interface $dask_interface"
+dask_worker_arguments="--no-dashboard --no-nanny --reconnect --nthreads $nThreads_per_worker --nworkers 1 --interface $dask_interface --scheduler-file $SCHEDULER_FILE"
+
+
