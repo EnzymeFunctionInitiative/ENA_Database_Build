6dfad4a405649f83aad8244cd4c192b4a35df369
Russell Davidson <rbdavid@illinois.edu>
2025-03-31 20:40:02 -0500

    add test to demonstrate undesired behavior of the location parsing function

--- tests/location_parsing_test.py
+++ tests/location_parsing_test.py
@@ -72,6 +72,17 @@ def test_circular_loc_parsing():
     # difficult bc join's ranges are unsorted, possibly indicating the locus
     # spans the circular chromosome structure's relative start position 
     # BUT! this test does not span the relative start boundary since one range
+    # does not include the chr_length, so just take the min and max of the 
+    # flattened ranges
+    line = "FT   CDS             join(complement(51..60),complement(1..45))"
+    matches = parse_embl.CDS_LOC_PATTERN.findall(line)
+    matches = [tuple(int(i) for i in tup) for tup in matches]
+    assert parse_embl.process_location_ranges(
+            matches, chr_struct, chromosome_length) == (1,60)
+
+    # difficult bc join's ranges are unsorted, possibly indicating the locus
+    # spans the circular chromosome structure's relative start position 
+    # BUT! this test does not span the relative start boundary since one range
     # does not include 1, so just take the min and max of the flattened ranges
     line = "FT   CDS             join(complement(4918..5163),complement(2691..4571))"
     matches = parse_embl.CDS_LOC_PATTERN.findall(line)
@@ -82,20 +93,21 @@ def test_circular_loc_parsing():
     # difficult bc join's ranges are unsorted, possibly indicating the locus
     # spans the circular chromosome structure's relative start position 
     # Both the chr_len and 1 are found in the list of ranges, indicating the
-    # protein spans the chromosome's relative start position.
+    # protein does span the chromosome's relative start position.
     line = "FT   CDS             join(complement(4918..5163),complement(1..45))"
     matches = parse_embl.CDS_LOC_PATTERN.findall(line)
     matches = [tuple(int(i) for i in tup) for tup in matches]
     assert parse_embl.process_location_ranges(
             matches, chr_struct, chromosome_length) == (4918,45)
-
+    
+    ###########################################################################
     # Both the chr_len and 1 are found in the list of ranges, but not in the
     # assumed ascending order that the code currently expects. This is a bug.
     line = "FT   CDS             join(complement(1..45),complement(4918..5163))"
     matches = parse_embl.CDS_LOC_PATTERN.findall(line)
     matches = [tuple(int(i) for i in tup) for tup in matches]
     assert parse_embl.process_location_ranges(
-            matches, chr_struct, chromosome_length) == (1,5163)
+            matches, chr_struct, chromosome_length) == (1,5163) # wrong!
 
     # Both the chr_len and 1 are found in the list of ranges, but not in the
     # assumed ascending order that the code currently expects. This is a bug.
@@ -103,6 +115,14 @@ def test_circular_loc_parsing():
     matches = parse_embl.CDS_LOC_PATTERN.findall(line)
     matches = [tuple(int(i) for i in tup) for tup in matches]
     assert parse_embl.process_location_ranges(
-            matches, chr_struct, chromosome_length) == (1,60)
+            matches, chr_struct, chromosome_length) == (1,60) # wrong!
+
+    # Both the chr_len and 1 are found in the list of ranges, but not in the
+    # assumed ascending order that the code currently expects. This is a bug.
+    line = "FT   CDS             join(complement(4918..5163),complement(51..60),complement(1..45))"
+    matches = parse_embl.CDS_LOC_PATTERN.findall(line)
+    matches = [tuple(int(i) for i in tup) for tup in matches]
+    assert parse_embl.process_location_ranges(
+            matches, chr_struct, chromosome_length) == (4918,45) # wrong!
 
 
